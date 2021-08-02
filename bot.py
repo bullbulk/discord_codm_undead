50843368695cfaecf9a7737ba7d3faffef32c19a
bullbulk <grur.ura@mail.ru>
2021-08-02 09:33:23 +0500

    added base bot model

---
+++ bot.py
@@ -0,0 +1,46 @@
+import traceback
+from datetime import datetime
+from typing import Dict
+
+import discord
+from discord.ext import commands
+from dotenv import dotenv_values
+
+from discord_slash import SlashCommand
+
+config: Dict[str, str] = dotenv_values('.env')
+
+
+class ZombieBot(commands.Bot):
+    last_message: str
+    owner: discord.User
+
+    def __init__(self, command_prefix, **options):
+        intents = discord.Intents.all()
+        super().__init__(command_prefix, intents=intents, **options)
+
+        # self.slash = SlashCommand(self, sync_commands=True, sync_on_cog_reload=True)
+
+    async def on_ready(self):
+        self.owner = await self.fetch_user(self.owner_id)
+
+    async def on_message(self, message: discord.Message):
+        if message.author.bot:
+            return
+        self.last_message = message.content
+        await self.process_commands(message)
+
+    async def on_command_error(self, ctx, exception):
+        tb = \
+            f'{"-" * 15}{datetime.now()}{"-" * 15}\n' \
+            f'Command: {self.last_message}\n' \
+            f'{"".join(traceback.format_exception(type(exception), exception, exception.__traceback__))}'
+
+        await self.owner.send(tb)
+
+    async def on_error(self, *args, **kwargs):
+        tb = \
+            f'{"-" * 15}{datetime.now()}{"-" * 15}\n' \
+            f'{traceback.format_exc()}'
+
+        await self.owner.send(tb)
